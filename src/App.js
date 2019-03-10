aa409f70c7dc3ca46b8df93ad0f7637632b384fd
YousefMohsen <yousefmohsen@outlook.dk>
2019-03-10 12:14:03 +0100

    init ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './Components/Game'
+import { Button, Heading } from 'grommet';
 
 class App extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      enterGame: false
+    }
+
+
+  }
+
+
+  renderContent() {
+    if (this.state.enterGame) {
+      return <Game />
+    }
+    else {
+
+      return (
+        <React.Fragment>
+
+
+          <Heading strong={true}
+            uppercase={true}
+            truncate={false}>
+            The Royale Game Of Ur
+</Heading>
+
+          <Button
+            color="white"
+            primary={true}
+            label="Start Game"
+            onClick={() => { this.setState({ enterGame: true }) }}
+          />
+        </React.Fragment>
+
+      )
+    }
+  }
   render() {
+    const content = this.renderContent()
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
+          {content}
         </header>
       </div>
     );
---
+++ src/Components/Game.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="game-container">
+    Welcome to the game
+      </div>
+    );
+  }
+}
+
+export default Game;
