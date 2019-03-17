183339586dcd5582b973f7b478c3ad079e65c328
YousefMohsen <yousefmohsen@outlook.dk>
2019-03-17 18:15:11 +0100

    end squares + working game play

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -11,7 +11,7 @@ import GameEngine from '../GameEngine';
 const initialState = {
     currentPlayer: 'w',
     dice: [0, 0, 0, 0],
-    diceResult: 1,
+    diceResult: 0,
     possibleMoves: { '0': 0 },
     board:
         [{ w: 7, b: 7 },
@@ -29,7 +29,7 @@ const initialState = {
         { w: 0, b: 0 },
         { w: 0, b: 0 },
         { w: 0, b: 0 },
-        { w: 0, b: 0 }]
+        { w: 5, b: 0 }]
 
 }
 
@@ -48,7 +48,8 @@ class Game extends Component {
             },
             selectedStone: false,
             diceResult: 0,
-            humanDidRollDice: false
+            humanDidRollDice: false,
+            computerDice: 0
         }
 
         this.ai = new RandomAI();
@@ -60,6 +61,7 @@ class Game extends Component {
             let state = this.gameEngine.getState();
             this.setState({ gameState: state });
         }
+
     }
 
     /* moveStoneToSquare(color, from, to) {
@@ -83,29 +85,59 @@ class Game extends Component {
             const stonesCount = this.state.gameState.board[0][color]
             let stones = [];
             for (let i = 0; i < stonesCount; i++) {
-                stones.push(<img onClick={() => console.log(color + '-stone')} src={color + "-stone.png"} style={{ marginRight: '2px', width: '50px', height: '50px' }} className={(this.state.humanData.stoneColor === color && this.state.selectedStone === 0 && i === 0 ? " stone-glow" : "")} />)
+                stones.push(<img className="boox" onClick={() => console.log(color + '-stone')} src={color + "-stone.png"} style={{ marginRight: '2px', width: '50px', height: '50px' }} className={(this.state.humanData.stoneColor === color && this.state.selectedStone === 0 && i === 0 ? " stone-glow" : "")} />)
             }
             return (
-                <div style={{ display: 'flex' }} onClick={() => { if (color === WHITE && this.state.gameState.currentPlayer === WHITE) { this.onSquareClick('0') } }}>
+                <div className="wrapper" onClick={() => { if (color === WHITE && this.state.gameState.currentPlayer === WHITE) { this.onSquareClick('0') } }}>
                     {stones}
                 </div>
             )
         }
     }
-    renderDiceRoller() {
-        return (
-            <div style={{ position: 'absolute', right: '20px', top: '25px' }}>
-                <Button
-                    color="white"
-                    primary={true}
-                    label="Roll dice"
-                    onClick={this.rollDice.bind(this)}
-                />
-                <h2>{this.state.diceResult}</h2>
 
-                {this.state.gameState && <p>{this.state.gameState.currentPlayer === 'w' ? 'your' : 'computer'} turn</p>}
-            </div>
-        )
+    renderFinishLineStones(color) {
+        //  console.log(color + "-stone.png")
+        if (this.state.gameState) {
+            // console.log(' this.state.gameState', this.state.gameState.board)
+            const stonesCount = this.state.gameState.board[15][color]
+            let stones = [];
+            for (let i = 0; i < stonesCount; i++) {
+                stones.push(<img className="box" src={color + "-stone.png"} style={{ marginRight: '2px', width: '50px', height: '50px' }} />)
+            }
+            return (
+                <div className="wrapper" ref={(c) => { this[color + '15'] = c }} onClick={() => this.onSquareClick(color + '15')}>
+                    {stones}
+                </div>
+            )
+        }
+    }
+    renderDiceRoller(color) {
+        if (this.state.gameState && color === WHITE) {
+            return (
+                <div >
+                    <Button
+                        color="white"
+                        primary={true}
+                        label="Roll dice"
+                        onClick={this.rollDice.bind(this)}
+                        disabled={this.state.humanDidRollDice}
+                    />
+                    <h2 style={{ margin: 0 }}>{this.state.diceResult}</h2>
+                    {this.state.gameState && <p style={{ margin: 0 }} >{this.state.gameState.currentPlayer === 'w' ? 'Your turn' : ''} </p>}
+                </div>
+            )
+        }
+
+        else if (this.state.computerDice) {
+            return (
+                <div >
+                    <p>Computer rolled</p>
+                    <h2 style={{ margin: 0 }}>{this.state.computerDice}</h2>
+                </div>
+
+            )
+        }
+
     }
     rollDice() {//dice are rolled in gameengine. Show dice first when user clicks roll dice 
         /*
@@ -114,31 +146,55 @@ class Game extends Component {
         //TODO: update game state*/
         //   this.makeAIMove();
         if (!this.state.humanDidRollDice) {
-            this.setState({ humanDidRollDice: true, diceResult: this.state.gameState.diceResult })
+            this.setState({ humanDidRollDice: true, diceResult: this.state.gameState.diceResult });
+            if (this.state.gameState.diceResult == 0) {
+
+                if (this.state.gameState.diceResult === 0 && this.state.gameState.currentPlayer === WHITE) {
+                    alert('You rolled a 0. Computer turn');
+                    const newState = this.gameEngine.voidTurn(WHITE);
+                    if (newState) {
+                        this.setState({ gameState: newState });
+                        this.endHumanTurn(newState);
+                    }
+                    else {
+                        console.log('something bad happend newState, this.stsate', newState, this.state)
+                    }
+
+
+
+                    return;
+                }
+
+            }
         }
     }
     componentDidUpdate(prevProps, prevState) {
         if (prevState.gameState !== this.state.gameState && this.state.gameState.currentPlayer === 'b') {
             this.makeAIMove();
         }
+        if (this.state.gameState && this.state.gameState.winner) {
+            this.handleGameHasEnded();
+        }
+
+
 
     }
 
     makeAIMove() {
         const currenGameState = this.state.gameState;
         if (!currenGameState.winner) {
-
+            //this.setState({computerDice: currenGameState.diceResult})
             const move = this.ai.calculateMove(currenGameState);
-            console.log('Ai move ', move)
+            // console.log('Ai move ', move)
             const newState = this.gameEngine.takeTurn(BLACK, move);
-            console.log('new state')
+            // console.log('new state')
             if (newState) {
                 /* if (newState.currentPlayer === BLACK) {
                      //Ai plays again
                      this.makeAIMove();
                      
                  }*/
-                this.setState({ gameState: newState, diceResult: currenGameState.diceResult });
+                this.setState({ gameState: newState, computerDice: currenGameState.diceResult });
                 //human turn
                 return true;
             }
@@ -159,20 +215,18 @@ class Game extends Component {
     makeHumanMove(move) {
         const currenGameState = this.state.gameState;
         if (!currenGameState.winner) {
-
+            //console.log('move',move)
             const newState = this.gameEngine.takeTurn(WHITE, move);
-            console.log('new state')
+
+            // console.log('new state',newState)
             if (newState) {
                 this.setState({ gameState: newState });
                 this.endHumanTurn(newState);
-                if (newState.currentPlayer === BLACK) {
-                    // this.makeAIMove();
-                }
 
                 return true;
             }
             alert('false state')
-            console.log('false StatecurrenGameState', currenGameState)
+            //  console.log('false StatecurrenGameState', currenGameState)
             return false;
 
 
@@ -183,10 +237,11 @@ class Game extends Component {
     }
 
     handleGameHasEnded() {
-        alert('game has ended');
+        const winnerMEssage = this.state.gameState.winner === 'w' ? 'Congratulations, you won!' : 'You lose.. :('
+        alert('game has ended! \n' + winnerMEssage);
     }
     selectStone(position) {//UI only action
-        console.log('in select stone', position);
+        // console.log('in select stone', position);
         if (this.state.selectedStone === position) {
             this.setState({ selectedStone: null });
         } else {
@@ -201,6 +256,7 @@ class Game extends Component {
     }
 
     onSquareClick(square) {
+        //  console.log('square',square)
         if (!this.state.humanDidRollDice) {
             console.log('you have to roll dice');
             return;
@@ -210,14 +266,14 @@ class Game extends Component {
             const gameState = this.state.gameState;
             const selectedStone = this.state.selectedStone;// ? parseInt(this.state.selectedStone): null;
             console.log('gameState.board[squareIndex][WHITE] ', gameState.board[squareIndex])
-            if (gameState.board[squareIndex][WHITE] === 1) {// check if square already has stone
-                console.log('in gameState.board[squareIndex][WHITE] === 1')
+            if (gameState.board[squareIndex][WHITE] === 1 && squareIndex != 15) {// check if square already has stone
+                //    console.log('in gameState.board[squareIndex][WHITE] === 1')
                 this.selectStone(squareIndex)
                 return;
             }
 
 
-            if (squareIndex <= selectedStone && gameState.board[squareIndex][WHITE] > 0) {//check if squareIndex is before selected square  
+            if (squareIndex <= selectedStone && gameState.board[squareIndex][WHITE] > 0 && squareIndex != 15) {//check if squareIndex is before selected square  
                 this.selectStone(squareIndex)
                 return;
             }
@@ -225,14 +281,14 @@ class Game extends Component {
                 //   console.log('in second if,this.state.selectedStone',selectedStone)
 
                 if (selectedStone || selectedStone === 0) {//if player has selected a stone
-                    console.log('in if selectedstone')
+                    //console.log('in if selectedstone')
 
-                    console.log('\nstate.possibleMoves', gameState.possibleMoves)
+                    // console.log('\nstate.possibleMoves', gameState.possibleMoves)
                     console.log('gameState.possibleMoves[this.state.selectedStone] =', gameState.possibleMoves[selectedStone], 'squareIndex', squareIndex)
                     if (gameState.possibleMoves[selectedStone] === squareIndex) {
                         const moveFrom = selectedStone;
                         this.makeHumanMove(moveFrom)
-                        console.log('in legal move', selectedStone)
+                        // console.log('in legal move', selectedStone)
 
                     }
                     else {
@@ -240,16 +296,16 @@ class Game extends Component {
                         this.selectStone(null);
                     }
 
-                    console.log('move from', selectedStone, 'moveto', squareIndex);
+                    // console.log('move from', selectedStone, 'moveto', squareIndex);
 
                 }
                 else if ((!selectedStone || selectedStone > 16) && gameState.board[squareIndex][WHITE] > 0) {// if no stone selected
-                    console.log('no stone selected', selectedStone)
+                    //console.log('no stone selected', selectedStone)
                     this.selectStone(squareIndex)
                 }
 
             }
-            console.log('squareIndex', squareIndex)
+            //  console.log('squareIndex', squareIndex)
             //check if possible move
             //this.selectStone(formatedSquare)
         }
@@ -289,7 +345,7 @@ class Game extends Component {
                     }
                     if (field.b > 0) {
                         const positionReference = index <= 4 || index >= 13 ? 'b' + index : index;
-                        console.log('positionReference ', positionReference)
+                        //   console.log('positionReference ', positionReference)
 
                         const square = this[positionReference] ? this[positionReference].getBoundingClientRect() : false;
 
@@ -315,13 +371,21 @@ class Game extends Component {
         return stonesOnBoard;
     }
 
+    renderNotOnBoardStones(color) {
+
+        return (
+            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                {this.renderUnmovedStones(color)}
+                {this.renderDiceRoller(color)}
+                {this.renderFinishLineStones(color)}
+            </div>)
+    }
 
     render() {
         console.log('render.this.state', this.state)
         const stonesOnBoard = this.positionDicesOnBoard();
         return (
             <div className="game-container">
-                {this.renderDiceRoller()}
                 <Button
                     color="white"
                     primary={true}
@@ -331,7 +395,8 @@ class Game extends Component {
                 />
 
 
-                {this.renderUnmovedStones('b')}
+                {this.renderNotOnBoardStones(BLACK)}
+
                 <SvgBoard
                     onSquareClick={this.onSquareClick.bind(this)}
                     setRef={(c, name) => {
@@ -339,10 +404,10 @@ class Game extends Component {
                         this[name] = c
                     }}
                 />
-
                 {stonesOnBoard}
+                {this.renderNotOnBoardStones(WHITE)}
 
-                {this.renderUnmovedStones('w')}
+
 
             </div>
         );
