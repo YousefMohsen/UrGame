9398bf037b5a65048fd0bdc57556c3457bddeae1
YousefMohsen <yousefmohsen@outlook.dk>
2019-04-23 16:24:05 +0200

    added tutorial

---
+++ src/Ai/TestAI.js
@@ -0,0 +1,315 @@
+const Ur = require("../GameEngine");
+const GameSimulator = require("./Tools/GameSimulator");
+const GilgameshAI = require("./GilgameshAI");
+const GudeaAI = require("./Gudea");
+const ActionEvalAi = require("./ActionEvalAi");
+
+const buildTree = require("./Tools/Tree");
+const AI = new GilgameshAI();
+//const gudea =  new GudeaAI();
+var util = require("util");
+const BLACK = "b";
+const WHITE = "w";
+
+const initState = {
+  currentPlayer: "w",
+  diceResult: 2,
+  possibleMoves: { "0": 2 },
+  board: [
+    { w: 7, b: 7 }, //#0 Start pile
+    { w: 0, b: 0 }, //1
+    { w: 0, b: 0 }, //2
+    { w: 0, b: 0 }, //3
+    { w: 0, b: 0 }, //#4 extra turn
+    { w: 0, b: 0 }, //5
+    { w: 0, b: 0 }, //6
+    { w: 0, b: 0 }, //7
+    { w: 0, b: 0 }, //8 extra turn
+    { w: 0, b: 0 }, //9
+    { w: 0, b: 0 }, //10
+    { w: 0, b: 0 }, //11
+    { w: 0, b: 0 }, //12
+    { w: 0, b: 0 }, //13
+    { w: 0, b: 0 }, //14  extra turn
+    { w: 0, b: 0 }//#15 Finishing pile
+  ]
+}; 
+
+const testState = {
+  currentPlayer: "w",
+  dice: [1, 0, 1, 1],
+  diceResult: 1,
+  possibleMoves: { "0": 1, "2": 3, "8": 9 },
+  board: [
+    { w: 5, b: 4 },
+    { w: 0, b: 0 },
+    { w: 1, b: 0 },
+    { w: 0, b: 0 },
+    { w: 0, b: 0 },
+    { w: 0, b: 1 },
+    { w: 0, b: 0 },
+    { w: 0, b: 1 },
+    { w: 1, b: 0 },
+    { w: 0, b: 1 },
+    { w: 0, b: 0 },
+    { w: 0, b: 0 },
+    { w: 0, b: 0 },
+    { w: 0, b: 0 },
+    { w: 0, b: 0 },
+    { w: 0, b: 0 }
+  ]
+};
+// boardUtility(testState)
+
+//boardUtility(parentState,initState);
+
+// Ur(stones?: number, dice?: number, player?: string)
+const game = new Ur(7, 4, Ur.BLACK);
+// default: stones = 7, dice = 4, player = Ur.WHITE
+const gameSimulator = new GameSimulator();
+let state = game.getState();
+//console.log(JSON.stringify(state))
+//state =game.takeTurn(state.currentPlayer,0)
+//state =game.takeTurn(state.currentPlayer,0)
+
+//let initState = require('./initState.json');
+//console.log('firststate',initState)
+
+//const gameSimulator = new GameSimulator();
+
+// takeTurn(state: Object, player: string, move?: string | number)
+//state =game.takeTurn(state.currentPlayer,0)
+//state =game.takeTurn(state.currentPlayer,0)
+//console.log('state1',state)
+//console.log('state.currentplayer',state.currentPlayer)
+//let possibleState  = gameSimulator.getPossibleState(state,state.currentPlayer,0,4);
+//console.log('possibleState.currentplayer',possibleState.currentPlayer)
+
+//console.log('possibleState',possibleState)
+
+function oneMove() {
+  const ai = new GilgameshAI(3);
+  const gudeaAi = new GudeaAI(3, BLACK); //new GilgameshAI(depth);
+  const game = new Ur(7, 4, Ur.BLACK);
+  // default: stones = 7, dice = 4, player = Ur.WHITE
+  let state = game.getState();
+//var fs = require('fs');
+
+//console.log('initState:after',initState)
+/*fs.writeFile('./src/Ai/initState.json', JSON.stringify(state), { flag: 'w' }, function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('File Saved!');
+
+});*/
+  for (let index = 0; index < 20; index++) { }
+  //console.log('initState:before',initState)
+  let counter = 0;
+  while (!(counter > 0 && state.currentPlayer === "b")) {
+    const rand = Math.floor(
+      Math.random() * Object.keys(state.possibleMoves).length
+    );
+
+    state = game.takeTurn(
+      state.currentPlayer,
+      Object.keys(state.possibleMoves)[rand]
+    );
+    counter++;
+  }
+  console.log("state.currentPlayer", state.currentPlayer);
+  const move = gudeaAi.calculateMove(state);
+  console.log("move", move);
+}
+for (let index = 2; index < 10; index++) {
+  //  const element = array[index];
+  //gudeaVsGilgamesh(10,index)
+  //randomVsAI(10,index);
+
+}
+
+
+gudeaVsGilgamesh(100, 1 )
+
+//randomVsAI(100,3);
+//oneMove()
+//console.log('state',state)
+//const tree = buildTree(state,2)
+//console.log('tree',tree)
+//var fs = require('fs');
+
+//console.log('initState:after',initState)
+/*fs.writeFile('./src/Ai/initState.json', JSON.stringify(state), { flag: 'w' }, function (err) {
+    if (err) {
+        console.log(err);
+    }
+    console.log('File Saved!');
+
+});*/
+//console.log('tree',util.inspect(tree))
+
+//printChildren(tree.root)
+
+function printChildren(node) {
+  node.children.map((child, i) => {
+    console.log("\n\nchild", i, "of ", node.depth, child.gameState, "\n\n");
+    printChildren(child);
+  });
+}
+// returns false on invalid input, new state otherwise
+// move is allowed to be undefined should there be no possible move
+
+// voidTurn(state: Objecct, player: string)
+//let aiMove = AI.calculateMove(state);
+
+//state =  game.voidTurn( Ur.BLACK);
+// returns false when not the current player is input, new state otherwise
+// useful for ending turns after a certain amount of time
+
+//console.log('state',state,'aiMove',aiMove)
+// possible state:
+// state.currentPlayer -> 'w'
+// state.diceResult -> 3
+// state.possibleMoves -> { '0': 3 }
+//console.log('s0state',state);
+//console.log('\ntate.diceResult',state.diceResult,'\n')
+
+
+function randomVsAI(games, depth) {
+  let blackWins = 0;
+  let whiteWins = 0;
+
+  const gudeaAi = new GudeaAI(depth, BLACK); //new GilgameshAI(depth);
+  // const gilgameshAi = new GilgameshAI(depth, BLACK); //black is default
+
+  for (let index = 0; index < games; index++) {
+    const game = new Ur(7, 4, Ur.WHITE);
+    let state = game.getState();
+
+    let lopX = 0;
+    while (!state.winner) {
+      //    lopX++;
+      // console.log('\nstate:before', state);
+
+      if (!state.possibleMoves) {
+        console.log(
+          "\nDidnt find any possibleMoves!",
+          state.possibleMoves,
+          "\n"
+        );
+      }
+      if (state.diceResult > 0 && Object.keys(state.possibleMoves).length > 0) {
+        const rand = Math.floor(Math.random() * Object.keys(state.possibleMoves).length);
+
+
+        const move = state.currentPlayer === BLACK ? gudeaAi.calculateMove(state) : Object.keys(state.possibleMoves)[rand];
+        state = game.takeTurn(state.currentPlayer, move);
+
+      } else {
+        state = game.voidTurn(state.currentPlayer);
+      }
+
+      // or game.takeTurn('w', 0);
+    }
+
+    if (state.winner === "w") {
+      whiteWins++;
+    } else if (state.winner === "b") {
+      blackWins++;
+    }
+    console.log(
+      index + 1,
+      "games played.  Gudea won::",
+      blackWins,
+      " Random won:",
+      whiteWins
+    );
+  }
+
+  console.log(
+    "Depth: ",
+    depth,
+    ", ",
+    " played games:",
+    games,
+    "  Gudea won::",
+    blackWins,
+    " Random won:",
+    whiteWins
+  );
+}
+function gudeaVsGilgamesh(games, depth) {
+  let blackWins = 0;
+  let whiteWins = 0;
+
+  const gudeaAi = new GudeaAI(depth, WHITE); //new GilgameshAI(depth);
+  const gilgameshAi = new ActionEvalAi(1, BLACK); //black is default
+
+  for (let index = 0; index < games; index++) {
+    const game = new Ur(7, 4, Ur.WHITE);
+    let state = game.getState();
+
+    let lopX = 0;
+    while (!state.winner) {
+      //    lopX++;
+      // console.log('\nstate:before', state);
+
+      if (!state.possibleMoves) {
+        console.log(
+          "\nDidnt find any possibleMoves!",
+          state.possibleMoves,
+          "\n"
+        );
+      }
+      if (state.diceResult > 0 && Object.keys(state.possibleMoves).length > 0) {
+        // const rand = Math.floor(Math.random() * Object.keys(state.possibleMoves).length);
+        const move =
+          state.currentPlayer === BLACK
+            ? gilgameshAi.calculateMove(state)
+            : gudeaAi.calculateMove(state);
+        state = game.takeTurn(state.currentPlayer, move);
+      } else {
+        state = game.voidTurn(state.currentPlayer);
+      }
+
+      // or game.takeTurn('w', 0);
+    }
+
+    if (state.winner === "w") {
+      whiteWins++;
+    } else if (state.winner === "b") {
+      blackWins++;
+    }
+    console.log(
+      index + 1,
+      "games played.  other AI won::",
+      blackWins,
+      " Gudea won:",
+      whiteWins
+    );
+  }
+
+  console.log(
+    "Depth: ",
+    depth,
+    ", ",
+    " played games:",
+    games,
+    "  Gilgamesh won::",
+    blackWins,
+    " Gudea won:",
+    whiteWins
+  );
+}
+
+//console.log('state.possibleMoves',console.log(Object.keys(state.possibleMoves)[0] ) )
+// takeTurn(player: string, move?: string | number)
+// returns false on invalid input
+// returns new state otherwise
+// move is allowed to be undefined should there be no possible move
+//console.log('\number og loops',lopX);
+
+// voidTurn(player: string)
+//game.voidTurn(Ur.BLACK);
+// returns false when not the current player is input, new state otherwise
+// useful for ending turns after a certain amount of time
--- src/App.js
+++ src/App.js
@@ -3,32 +3,130 @@ import logo from './logo.svg';
 import './App.css';
 import Game from './Components/Game'
 import { Button, Heading } from 'grommet';
-
+import Cookies from 'js-cookie';
+const stepsText = ["Ur is a game for two players, with 7 stones each",
+  "At the start of each turn four binary dice are thrown by the player, where the outcome of the dice roll (a number between 0 and 4) determines the number of squares that the player can move on the board",
+  "This is the board. The aim of the game is to get all of your pieces all the way around. If an enemy piece lands on your piece then your piece is sent home.",
+  "There are 5 rosette squares. If you land on a rosetta square you get another throw of the dice and your piece is safe from enemy attack."]
+const stepsImages = ["/stonesTut1.png", "/diceTut1.png", "/boardTut1.png", "/boardTut2.png"]
+// stones, dice,board, lucky stones
 class App extends Component {
   constructor(props) {
     super()
     this.state = {
-      enterAIMenu: true,
-      enterGame: true,
-      aiType: 'gilgamesh'
+      enterAIMenu: false,
+      enterGame: false,
+      aiType: 'gilgamesh',
+      didShowTutorial:
+        typeof window !== 'undefined'
+          ? Cookies.get('did-show-tutorial')
+          : true,
+      step: 0,
+      onAboutPage: false
     }
 
 
   }
 
-
+  onTutorialEnd() {
+    //  this.setState({ slideOutAnimation: true });
+    // Cookies.set('did-show-tutorial', ' ', { expires: 365 });
+    setTimeout(() => {
+      //this.setState({ displayWarning: false });
+    }, 1000);
+  }
   renderContent() {
+    const isMobile = window.innerWidth <= 500;
+
+    if (isMobile) {
+
+      return (
+        <React.Fragment>
+
+
+          <img className="App-logo" src="w-stone.png" />
+          <h1 style={{ display: 'block', fontSize: '2rem' }}>Your screen is too small :/ </h1>
+        </React.Fragment>
+      )
+    }
+    if (this.state.onAboutPage) {
+
+      return (
+        <div>
+
+          <Button
+            color="white"
+            primary={true}
+            label="Back"
+            onClick={() => { this.setState({ onAboutPage: false }) }}
+            style={{ position: 'absolute', left: '14px', top: '14px' }}
+          />
+
+
+
+          <figure>
+            <img src="originalBoard.jpg" />
+            <figcaption>
+
+              <p style={{ marginTop: 0, marginBottom: '20px', fontSize: '0.7em' }}>One of the five gameboards found by Sir Leonard Woolley in the Royal Cemetery at Ur, now held in the <a style={{ color: "white" }} href="https://www.britishmuseum.org/research/collection_online/collection_object_details.aspx?objectId=8817&partId=1">British Museum</a></p>
+
+            </figcaption>
+          </figure>
+
+
+          <p style={{ padding: '0 10%', textAlign: 'left' }}>The Royal Game of Ur is a 4500-year-old board game originating from Mesopotamia (modern day Iraq). Its use spread to several cities across the ancient world. Examples of the game were found in Crete, Cyprus, Egypt, Jewish communities in India and other places.
+       <br /> <br /> A cuneiform clay tablet -with an inscription of the rules was found in Babylon and translated from Akkadian by the British Museum, which published the set of rules online.
+           This implementation of the Game of Ur is based on the set of rules that is published by the British Museum.
+      </p>
+        </div>
+
+      )
+    }
+
     if (this.state.enterGame) {
+      // const didShowTutorial = false;
+      if (!this.state.didShowTutorial) {
+        return (
+          <React.Fragment>
+            <p style={{ padding: '0 10%', textAlign: 'left' }}>{stepsText[this.state.step]}</p>
+
+            <img src={stepsImages[this.state.step]} />
+            <div style={{ width: '30%', display: "flex" }}>
+
+              <Button
+                color="white"
+                primary={true}
+                label={this.state.step < stepsText.length - 1 ? 'Next' : 'Start game'}
+                onClick={() => {
+
+                  if (this.state.step < stepsText.length - 1) {
+                    this.setState({ step: this.state.step + 1 })
+
+                  }
+                  else {
+                    this.setState({ step: 0, didShowTutorial: true });
+                    this.onTutorialEnd()
+
+                  }
+                }}
+                style={{ margin: '20px 0px', display: 'block', width: '100%' }}
+              />
+
+
+            </div>
+          </React.Fragment>
+        )
+      }
       return <Game aiType={this.state.aiType} goBack={() => { this.setState({ enterGame: false, enterAIMenu: false }) }} />
     }
     if (this.state.enterAIMenu) {
       return (
 
-        <div>
+        <div style={{ width: '30%' }}>
 
           <img className="App-logo" src="w-stone.png" />
 
-          <h1>Choose an AI</h1>
+          <h2>Choose an opponent</h2>
 
           <Button
             color="white"
@@ -46,14 +144,14 @@ class App extends Component {
             style={{ margin: '20px 0px', display: 'block', width: '100%' }}
           />
 
-          <Button
+          {   /* <Button
             color="white"
             primary={true}
             label='Nabu (x AI)'
             onClick={() => { this.setState({ enterGame: true, aiType: 'nabu' }) }}
             style={{ margin: '20px 0px', display: 'block', width: '100%' }}
 
-          />
+          />*/}
 
           <Button
             color="white"
@@ -79,15 +177,31 @@ class App extends Component {
     }
     return (
       <React.Fragment>
+
+
         <img className="App-logo" src="w-stone.png" />
         <h1 style={{ display: 'block' }}>The Royale Game Of Ur </h1>
-        <Button
-          color="white"
-          primary={true}
-          label="Start Game"
-          onClick={() => { this.setState({ enterAIMenu: true, }) }}
-        />
+        <div style={{ width: '30%' }}>
+
+          <Button
+            style={{ margin: '20px 0px', display: 'block', width: '100%' }}
+
+            color="white"
+            primary={true}
+            label="Start Game"
+            onClick={() => { this.setState({ enterAIMenu: true, }) }}
+          />
+          <Button
+            style={{ margin: '20px 0px', display: 'block', width: '100%' }}
+
+            color="white"
+            primary={true}
+            label="About"
+            onClick={() => { this.setState({ onAboutPage: true, }) }}
+          />
+        </div>
       </React.Fragment>
+
     )
   }
 
