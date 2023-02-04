82f43548b496f7e6678ab76893a43ad3c66414d7
Yousef <yousefmohsen@outlook.dk>
2023-02-04 20:19:20 +0100

    Updates

---
+++ precache-manifest.3c7e88fd1ca84614932a4046a33cf6cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5c4627b68d246e84a38",
+    "url": "/UrGame/static/css/main.8752b0de.chunk.css"
+  },
+  {
+    "revision": "b5c4627b68d246e84a38",
+    "url": "/UrGame/static/js/main.eedfa845.chunk.js"
+  },
+  {
+    "revision": "704d92f60f6dd4ae3700",
+    "url": "/UrGame/static/js/runtime~main.4e11c1fc.js"
+  },
+  {
+    "revision": "4596e7511bb05354d21a",
+    "url": "/UrGame/static/js/2.9858aa5d.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/UrGame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8a8f0c126d15dd7812b1a44bdc3a8701",
+    "url": "/UrGame/index.html"
+  }
+];
